6c4a3ef98f8ff8441c5f49c36bb7a4f469a5ec75
dongwhi09 <dongwhi09@naver.com>
2024-09-05 23:21:56 +0900

    공지 기능 삭제

--- src/main/java/modi/modurang/repository/AnnouncementRepository.java
+++
@@ -1,8 +0,0 @@
-package modi.modurang.repository;
-
-import modi.modurang.entity.Announcement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
-
-}
