cb9f897b511c116210b5bd401263e6fe6e3d04b0
dongwhi09 <dongwhi09@naver.com>
2024-09-17 08:38:16 +0900

    fix: 브랜치 병합 후 사라진 코드 복구

--- src/main/java/modi/modurang/ModurangApplication.java
+++ src/main/java/modi/modurang/ModurangApplication.java
@@ -1,10 +1,16 @@
 package modi.modurang;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(servers = {
+        @Server(description = "https://0d82-221-168-22-204.ngrok-free.app")
+})
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
