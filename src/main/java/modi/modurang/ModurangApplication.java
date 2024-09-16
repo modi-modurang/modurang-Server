4193ec454191a67611908b5931a4622129658032
dongwhi09 <dongwhi09@naver.com>
2024-09-16 20:47:48 +0900

    fix: 배포 가능하도록 수정함

--- src/main/java/modi/modurang/ModurangApplication.java
+++ src/main/java/modi/modurang/ModurangApplication.java
@@ -1,14 +1,11 @@
 package modi.modurang;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-
-@OpenAPIDefinition(servers = {
-        @Server(url = "/", description = "https://0d82-221-168-22-204.ngrok-free.app")
-})
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -16,5 +13,35 @@ public class ModurangApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ModurangApplication.class, args);
+    }
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.load();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(Dotenv dotenv) {
+        return args -> {
+            String dbUrl = dotenv.get("DATABASE_URL");
+            String dbUsername = dotenv.get("DATABASE_USERNAME");
+            String dbPassword = dotenv.get("DATABASE_PASSWORD");
+            String emailUsername = dotenv.get("EMAIL_USERNAME");
+            String emailPassword = dotenv.get("EMAIL_PASSWORD");
+            String jwtAccessTokenSecretKey = dotenv.get("JWT_ACCESS_TOKEN_SECRET_KEY");
+            String jwtAccessTokenExpiration = dotenv.get("JWT_ACCESS_TOKEN_EXPIRATION");
+            String jwtRefreshTokenSecretKey = dotenv.get("JWT_REFRESH_TOKEN_SECRET_KEY");
+            String jwtRefreshTokenExpiration = dotenv.get("JWT_REFRESH_TOKEN_EXPIRATION");
+
+            System.out.println("Database URL: " + dbUrl);
+            System.out.println("Database Username: " + dbUsername);
+            System.out.println("Database Password: " + dbPassword);
+            System.out.println("Email Username: " + emailUsername);
+            System.out.println("Email Password: " + emailPassword);
+            System.out.println("JWT Access Token Secret Key: " + jwtAccessTokenSecretKey);
+            System.out.println("JWT Access Token Expiration: " + jwtAccessTokenExpiration);
+            System.out.println("JWT Refresh Token Secret Key: " + jwtRefreshTokenSecretKey);
+            System.out.println("JWT Refresh Token Expiration: " + jwtRefreshTokenExpiration);
+        };
     }
 }
