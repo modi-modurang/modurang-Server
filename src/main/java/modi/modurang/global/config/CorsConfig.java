80980c48d657b7b0060af2132ebfe484caefc3b4
dongwhi09 <dongwhi09@naver.com>
2024-09-19 16:47:22 +0900

    fix: corsConfig 적용 안되는 문제 해결

--- src/main/java/modi/modurang/global/config/CorsConfig.java
+++ src/main/java/modi/modurang/global/config/CorsConfig.java
@@ -5,11 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsMvcConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
