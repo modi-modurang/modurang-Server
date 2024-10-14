3247be20456bfb591722fe2fa16a775c35d8522c
dongwhi09 <dongwhi09@naver.com>
2024-10-14 20:44:58 +0900

    chore: 코드 리펙토링

--- src/main/java/modi/modurang/ModurangApplication.java
+++ src/main/java/modi/modurang/ModurangApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -15,7 +14,6 @@ import reactor.core.publisher.Mono;
         @Server(url = "/", description = "https://d2ad-221-168-22-204.ngrok-free.app/")
 })
 
-@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ModurangApplication {
---
+++ src/main/java/modi/modurang/global/config/jpa/JpaConfig.java
@@ -0,0 +1,10 @@
+package modi.modurang.global.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
