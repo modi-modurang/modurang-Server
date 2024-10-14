cd7f9e6b16c16478f69b19796565d54b96295667
dongwhi09 <dongwhi09@naver.com>
2024-10-14 20:22:08 +0900

    chore: 이메일 인증 기능 html 파일로 만들었음

--- src/main/java/modi/modurang/domain/email/service/EmailService.java
+++ src/main/java/modi/modurang/domain/email/service/EmailService.java
@@ -1,9 +1,10 @@
 package modi.modurang.domain.email.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.internet.MimeMultipart;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import modi.modurang.domain.email.repository.EmailRepository;
@@ -12,8 +13,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -21,7 +24,6 @@ import java.time.LocalDateTime;
 @Service
 @RequiredArgsConstructor
 public class EmailService {
-
     private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender javaMailSender;
     private final MailConfig mailConfig;
@@ -30,45 +32,24 @@ public class EmailService {
     public void sendEmail(String email, String code) {
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             message.setFrom(mailConfig.getUsername());
             message.setRecipients(MimeMessage.RecipientType.TO, email);
             message.setSubject("모두랑 이메일 인증코드 : " + code);
 
-            MimeMultipart multipart = new MimeMultipart();
-            multipart.addBodyPart(createMessageBodyPart(code));
-            multipart.addBodyPart(createImageBodyPart());
+            ClassPathResource resource = new ClassPathResource("/static/verify.html");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8));
+            String content = FileCopyUtils.copyToString(reader);
+            content = content.replace("{code}", code);
 
-            message.setContent(multipart);
+            helper.setText(content, true);
+
+            helper.addInline("image", new ClassPathResource("static/모두랑 인증코드 이미지.png"));
 
             javaMailSender.send(message);
         } catch (MessagingException | IOException e) {
             logger.error("{}에게 인증 이메일 전송 실패: {}", email, e.getMessage());
         }
-    }
-
-    private MimeBodyPart createMessageBodyPart(String code) throws MessagingException {
-        MimeBodyPart messageBodyPart = new MimeBodyPart();
-        String htmlContent = "<div style='text-align: center;'>"
-                + "<img src='cid:image' style='width: 150px;'><br><br>"
-                + "<p style='font-size: 16px; font-weight: bold;'>모두랑 이메일 인증을 위해 아래 코드를 입력하세요</p>"
-                + "<h1 style='font-size: 48px; margin: 20px 0; letter-spacing: 10px;'>" + code + "</h1>"
-                + "<p style='font-size: 14px;'>코드는 5분 후 만료됩니다.</p>"
-                + "<p style='font-size: 10px; line-height: 1.6; margin: 20px 0;'>만약 이 요청을 본인이 하지 않았다면, 이 메일을 무시하셔도 됩니다.</p>"
-                + "<div style='text-align: center; margin-top: 30px; font-size: 12px; color: #999;'>"
-                + "© 2024 Modurang. All rights reserved."
-                + "</div>"
-                + "</div>";
-        messageBodyPart.setContent(htmlContent, "text/html; charset=UTF-8");
-        return messageBodyPart;
-    }
-
-    private MimeBodyPart createImageBodyPart() throws MessagingException, IOException {
-        MimeBodyPart imagePart = new MimeBodyPart();
-        ClassPathResource resource = new ClassPathResource("static/모두랑 인증코드 이미지.png");
-        imagePart.attachFile(resource.getFile());
-        imagePart.setContentID("<image>");
-        imagePart.setDisposition(MimeBodyPart.INLINE);
-        return imagePart;
     }
 
     @Scheduled(cron = "0 0 * * * *")
