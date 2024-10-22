2b9a3730aa98a06de150a9b1e95e04afa58c5e30
dongwhi09 <dongwhi09@naver.com>
2024-10-22 15:51:29 +0900

    fix: 오타 수정

--- src/main/java/modi/modurang/domain/auth/dto/request/ChangePasswordRequest.java
+++ src/main/java/modi/modurang/domain/auth/dto/request/ChangePasswordRequest.java
@@ -13,5 +13,5 @@ public class ChangePasswordRequest {
 
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}$", message = "비밀번호는 영어와 숫자를 포함한 8자에서 20자 사이여야 합니다.")
     @NotBlank(message = "새 비밀번호를 입력해 주세요.")
-    private String password;
+    private String newPassword;
 }
