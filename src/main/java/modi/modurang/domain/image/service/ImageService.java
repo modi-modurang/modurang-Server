3bc4da479d9d74308983c0111439608229442db9
dongwhi09 <dongwhi09@naver.com>
2024-11-01 19:49:52 +0900

    refactor: 코드 리펙토링

--- src/main/java/modi/modurang/domain/image/service/ImageService.java
+++ src/main/java/modi/modurang/domain/image/service/ImageService.java
@@ -1,40 +1,10 @@
 package modi.modurang.domain.image.service;
 
-import lombok.RequiredArgsConstructor;
-import modi.modurang.domain.image.entity.Image;
-import modi.modurang.domain.image.repository.ImageRepository;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
-@Service
-@RequiredArgsConstructor
-public class ImageService {
+public interface ImageService {
 
-    private final ImageRepository imageRepository;
-
-    @Value("${spring.servlet.multipart.location}")
-    private String uploadDir;
-
-    public String saveImage(MultipartFile file) throws IOException {
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-
-        File destinationFile = new File(uploadDir, fileName);
-
-        file.transferTo(destinationFile);
-
-        Image image = Image.builder()
-                .fileName(fileName)
-                .url("/image/view/" + fileName)
-                .size(file.getSize())
-                .mimeType(file.getContentType())
-                .build();
-
-        imageRepository.save(image);
-
-        return image.getUrl();
-    }
+    String saveImage(MultipartFile file) throws IOException;
 }
--- src/main/java/modi/modurang/global/exception/CustomExceptionHandler.java
+++ src/main/java/modi/modurang/global/exception/CustomExceptionHandler.java
@@ -1,13 +1,8 @@
 package modi.modurang.global.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -15,14 +10,5 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
         return ErrorResponseEntity.of(e.getErrorCode());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put("message", error.getDefaultMessage())
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
