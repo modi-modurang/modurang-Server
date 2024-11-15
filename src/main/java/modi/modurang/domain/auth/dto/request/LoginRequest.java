887db6e5acf8ec5c980a03a6618029e4145d22d3
dongwhi09 <dongwhi09@naver.com>
2024-11-15 14:56:50 +0900

    chore: edit password regular expression

--- src/main/java/modi/modurang/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/modi/modurang/domain/auth/dto/request/LoginRequest.java
@@ -12,7 +12,7 @@ public class LoginRequest {
     @NotBlank(message = "이메일을 입력해 주세요.")
     private String email;
 
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}$", message = "비밀번호는 영어와 숫자를 포함한 8자에서 20자 사이여야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,20}$", message = "비밀번호는 영어와 숫자를 포함하고 8자 이상이여야 합니다.")
     @NotBlank(message = "비밀번호를 입력해 주세요.")
     private String password;
 }
