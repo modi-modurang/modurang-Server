25b85b7a3b23d92517ffc7fcdd6486f04744b1d4
dongwhi09 <dongwhi09@naver.com>
2024-11-15 16:16:42 +0900

    refactor: fix CustomExceptionHandler

--- src/main/java/modi/modurang/global/error/ErrorResponse.java
+++ src/main/java/modi/modurang/global/error/ErrorResponse.java
@@ -1,9 +1,23 @@
 package modi.modurang.global.error;
 
 import org.springframework.http.ResponseEntity;
+import java.util.Map;
 
-public record ErrorResponse(int status, String message, String code) {
+public record ErrorResponse(
+        int status,
+        String message,
+        String code,
+        Map<String, String> errors
+) {
     public static ResponseEntity<ErrorResponse> of(CustomError error) {
-        return ResponseEntity.status(error.getStatus()).body(new ErrorResponse(error.getStatus(), error.getMessage(), error.getCode()));
+        return ResponseEntity
+                .status(error.getStatus())
+                .body(new ErrorResponse(error.getStatus(), error.getMessage(), error.getCode(), null));
+    }
+
+    public static ResponseEntity<ErrorResponse> of(int status, String message, String code, Map<String, String> errors) {
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorResponse(status, message, code, errors));
     }
 }
---
+++ src/main/java/modi/modurang/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package modi.modurang.global.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        return ErrorResponse.of(e.getError());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ErrorResponse.of(
+                HttpStatus.BAD_REQUEST.value(),
+                "",
+                "VALIDATION_ERROR",
+                errors
+        );
+    }
+}
