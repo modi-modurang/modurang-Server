6430742624d8a7a6115e92f665d62157a6b6a5de
dongwhi09 <dongwhi09@naver.com>
2024-11-25 14:13:01 +0900

    fix: 필요없는 설정 삭제

--- src/main/java/modi/modurang/global/config/email/EmailConfig.java
+++ src/main/java/modi/modurang/global/config/email/EmailConfig.java
@@ -33,18 +33,6 @@ public class EmailConfig {
     @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
     private boolean starttlsEnable;
 
-    @Value("${spring.mail.properties.mail.smtp.starttls.required}")
-    private boolean starttlsRequired;
-
-    @Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
-    private int connectionTimeout;
-
-    @Value("${spring.mail.properties.mail.smtp.timeout}")
-    private int timeout;
-
-    @Value("${spring.mail.properties.mail.smtp.writetimeout}")
-    private int writeTimeout;
-
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -62,10 +50,6 @@ public class EmailConfig {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", String.valueOf(auth));
         properties.put("mail.smtp.starttls.enable", String.valueOf(starttlsEnable));
-        properties.put("mail.smtp.starttls.required", String.valueOf(starttlsRequired));
-        properties.put("mail.smtp.connectiontimeout", String.valueOf(connectionTimeout));
-        properties.put("mail.smtp.timeout", String.valueOf(timeout));
-        properties.put("mail.smtp.writetimeout", String.valueOf(writeTimeout));
         properties.put("mail.smtp.ssl.trust", host);
         properties.put("mail.transport.protocol", "smtp");
         properties.put("mail.smtp.port", String.valueOf(port));
