03c28471bf2b5abd8ea50e85d0206f113bf27ece
dongwhi09 <dongwhi09@naver.com>
2024-11-29 13:59:36 +0900

    fix: RedisConfig

--- src/main/java/modi/modurang/global/config/redis/RedisConfig.java
+++ src/main/java/modi/modurang/global/config/redis/RedisConfig.java
@@ -1,13 +1,32 @@
 package modi.modurang.global.config.redis;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+
+        configuration.setHostName(redisProperties.getHost());
+        configuration.setPort(redisProperties.getPort());
+        configuration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+
+        return new LettuceConnectionFactory(configuration);
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
