11c01dda01c32ab0565b9994206296a1c86c7a45
dongwhi09 <dongwhi09@naver.com>
2024-12-26 19:16:42 +0900

    chore: 가독성 개선

--- src/main/java/modi/modurang/global/config/WebMvcConfig.java
+++ src/main/java/modi/modurang/global/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/modi/modurang/global/error/ErrorResponse.java
+++ src/main/java/modi/modurang/global/error/ErrorResponse.java
@@ -12,10 +12,20 @@ public record ErrorResponse(
     public static ResponseEntity<ErrorResponse> of(CustomError error) {
         return ResponseEntity
                 .status(error.getStatus())
-                .body(new ErrorResponse(error.getStatus(), error.getMessage(), error.getCode(), null));
+                .body(new ErrorResponse(
+                        error.getStatus(),
+                        error.getMessage(),
+                        error.getCode(),
+                        null
+                ));
     }
 
-    public static ResponseEntity<ErrorResponse> of(int status, String message, String code, Map<String, String> errors) {
+    public static ResponseEntity<ErrorResponse> of(
+            int status,
+            String message,
+            String code,
+            Map<String, String> errors
+    ) {
         return ResponseEntity
                 .status(status)
                 .body(new ErrorResponse(status, message, code, errors));
