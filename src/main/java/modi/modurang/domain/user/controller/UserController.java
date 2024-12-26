f88cacfc0ca73319b3eb27cbd46e115da9233c29
dongwhi09 <dongwhi09@naver.com>
2024-12-26 19:55:14 +0900

    feat: CurrentUser 어노테이션 추가

--- src/main/java/modi/modurang/domain/user/controller/UserController.java
+++ src/main/java/modi/modurang/domain/user/controller/UserController.java
@@ -1,26 +1,24 @@
 package modi.modurang.domain.user.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import modi.modurang.domain.user.dto.response.UserResponse;
-import modi.modurang.domain.user.service.UserService;
-import modi.modurang.global.common.BaseResponse;
-import org.springframework.http.ResponseEntity;
+import modi.modurang.domain.user.entity.User;
+import modi.modurang.global.security.annotation.CurrentUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "user", description = "유저 API")
 @RestController
-@RequestMapping("/user")
-@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
-    private final UserService userService;
 
-    @Operation(summary = "내 정보 조회")
     @GetMapping("/me")
-    public ResponseEntity<BaseResponse<UserResponse>> getMe() {
-        return BaseResponse.of(userService.getMe());
+    public UserResponse getMe(@CurrentUser User user) {
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getStudentNumber(),
+                user.getEmail(),
+                user.getClub()
+        );
     }
 }
