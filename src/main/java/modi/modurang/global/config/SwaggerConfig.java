0e29902dcc4a40e219d873c80c5eaade7e93bb76
dongwhi09 <dongwhi09@naver.com>
2024-12-27 10:42:56 +0900

    refactor: 코드 리팩토링

--- src/main/java/modi/modurang/global/config/SwaggerConfig.java
+++ src/main/java/modi/modurang/global/config/SwaggerConfig.java
@@ -5,30 +5,40 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI api() {
         return new OpenAPI()
-                .info(
-                        new Info().title("Modurang-Server API")
-                                .description("JAVA API")
-                                .version("1.0.0")
+                .info(new Info()
+                        .title("Modurang-Server API")
+                        .description("JAVA API")
+                        .version("1.0.0")
                 )
-                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
-                .components(
-                        new Components()
-                                .addSecuritySchemes("Authorization",
-                                        new SecurityScheme()
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("Authorization")
-                                                .in(SecurityScheme.In.HEADER)
-                                                .name("Authorization")
-                                ));
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("Authorization")
+                )
+                .servers(Collections.singletonList(
+                        new Server()
+                                .url("https://api.modurang.com")
+                ))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "Authorization",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("Authorization")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")
+                        )
+                );
     }
 }
